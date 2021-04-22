f3c0173b395b0d9354e1d52a74a81fecbb202c6e
Keith March Mistler <kmistler@gmail.com>
2021-04-21 20:43:37 -0400

    began index.pug file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const synopsis = require('./showdata.js')
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  response.render('index', { synopsis })
+})
+
+app.all('*'), (request, response) => {
+  return request.sendStatus(404)
+}
+
+app.listen(8900, () => {
+  console.log('Listening on 8900...') // eslint-disable-line no-console
+})
