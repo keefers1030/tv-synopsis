72bbfa2821910b4a69c5e04b358b0120cb218e4c
Keith March Mistler <kmistler@gmail.com>
2021-04-22 11:22:21 -0400

    added table for seasons.pug

--- index.js
+++ index.js
@@ -3,25 +3,24 @@ const showdata = require('./showdata.js')
 
 const app = express()
 
-app.set('view engine', 'pug')
-
 app.use(express.static('public'))
+app.set('view engine', 'pug')
 
 app.get('/', (request, response) => {
   response.render('index', { showdata })
 })
 
 app.get('/seasons/:number', (request, response) => {
-  const showseason = showdata.seasons.find((season) => { 
-    return showdata.seasons.number === parseInt(request.params.number)
+  const showseason = showdata.seasons.find((seasons) => {
+    return seasons.number === parseInt(request.params.number)
   })
 
   return response.render('seasons', { season: showseason, 'title': showdata.title })
 })
 
-app.all('*'), (request, response) => {
+app.all('*', (request, response) => {
   return request.sendStatus(404)
-}
+})
 
 app.listen(8900, () => {
   console.log('Listening on 8900...') // eslint-disable-line no-console
